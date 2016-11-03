610ccf129cb5c28aa00a46b0fb31ba0bf6c3b782
Håvard Ola Eggen <havard.ola.eggen@gmail.com>
2016-11-03 17:13:17 +0100

    Tranformed picture to a new matrix representing all pixels. Started clustering with kmeans.

--- cluster.py
+++ cluster.py
@@ -1,8 +1,62 @@
 #!/usr/bin/env python3
 
 # Module for performing clustering, classification etc. for the ColourApp
+from scipy import misc
+from scipy.cluster.vq import vq, kmeans, whiten
+import matplotlib.pyplot as plt
+import numpy as np
+
+#f = misc.face()  # retrieve a grayscale image
+#plt.imshow(f)
+#plt.show()
+
+
+face = misc.face()
+#misc.imsave('face.png', face) # First we need to create the PNG file
+#face = misc.imread('face.png')
+
+# Not vital in any way
+#print(type(face))
+#print(face[0])
+#print('\n')
+#print(face[0][0])
+#print('\n')
+#print(face[0][0][0])
+#print(face.shape)
+
+# Np array of length*width high and RGB wide, all zero
+dimensions = face.shape
+pictureArr = np.zeros(shape=(dimensions[0]*dimensions[1], dimensions[2]))
+
+# Iterates over the picture and stores the values in the numpy array.
+workaround = 0
+k=0
+for i in range(dimensions[0]):
+    for j in range(dimensions[1]):
+        pictureArr[workaround] = [face[i][j][k], face[i][j][k+1], face[i][j][k+2]]
+        workaround+=1
+
+# Does magic with the picture and returns cluster centers based on the wanted K.
+    # Whiten is beneficial, why? 
+whitened = whiten(pictureArr)
+kVal = 4
+magic = kmeans(whitened, kVal)
+print(magic)
+
+
+
+
+
+
+
 
 def cluster(im, k=0):
+
+    #face = misc.face()
+    #misc.imsave('face.png', face)
+    #face = misc.imread('face.png')
+
+
     """
     Return clusters (array of centers) and classified image
     """
