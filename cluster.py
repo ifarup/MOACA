b2eaf9e1595b4d9c334ac40e3bac97b9cb6c13fd
Håvard Ola Eggen <havard.ola.eggen@gmail.com>
2016-11-04 14:22:26 +0100

    Hopefully a functional function.

--- cluster.py
+++ cluster.py
@@ -2,69 +2,40 @@
 
 # Module for performing clustering, classification etc. for the ColourApp
 from scipy import misc
-from scipy.cluster.vq import vq, kmeans, kmeans2, whiten
-import matplotlib.pyplot as plt
+from scipy.cluster.vq import kmeans2, whiten
 import numpy as np
 
-#f = misc.face()  # retrieve a grayscale image
-#plt.imshow(f)
-#plt.show()
-
-# Reads a image from a directory or use the generic "face"
-#face = misc.face()
-face = misc.imread("images/asdfghjk.png")
-print(face.shape)
-
-#misc.imsave('face.png', face) # First we need to create the PNG file
-#face = misc.imread('face.png')
-
-# Not vital in any way
-#print(type(face))
-#print(face[0])
-#print('\n')
-#print(face[0][0])
-#print('\n')
-#print(face[0][0][0])
-#print(face.shape)
-
-# Np array of length*width high and RGB wide, all zero
-dimensions = face.shape
-pictureArr = np.zeros(shape=(dimensions[0]*dimensions[1], dimensions[2]))
-
-
-# Iterates over the picture and stores the values in the numpy array.
-workaround = 0
-k=0
-for i in range(dimensions[0]):
-    for j in range(dimensions[1]):
-        pictureArr[workaround] = [face[i][j][k], face[i][j][k+1], face[i][j][k+2]]
-        workaround+=1
-
-
-# Does magic with the picture and returns cluster centers based on the wanted K.
-    # Whiten is beneficial, why?
-whitened = whiten(pictureArr)
-kVal = 4
-arr,lab = kmeans2(whitened, kVal)
-print(arr)
-for i in range(10):
-    print(lab[i])
-
-
-
-
-
-
-
-
 def cluster(im, k=0):
+    # Reads a image from a directory or use the generic "face"
+    # face = misc.face()
+    # face = misc.imread("images/asdfghjk.png")
 
-    #face = misc.face()
-    #misc.imsave('face.png', face)
-    #face = misc.imread('face.png')
+    # Np array of length*width high and RGB wide, all zero
+    dimensions = im.shape
+    pictureArr = np.zeros(shape=(dimensions[0] * dimensions[1], dimensions[2]))
 
+    # Iterates over the picture and stores the values in the numpy array.
+    workaround = 0
+    dim = [0,1,2]
+    for i in range(dimensions[0]):
+        for j in range(dimensions[1]):
+            pictureArr[workaround] = [im[i][j][dim[0]], im[i][j][dim[1]], im[i][j][dim[2]]]
+            workaround += 1
 
-    """
-    Return clusters (array of centers) and classified image
-    """
-    return
+    # For future develompent implement whiten for better clustering.
+    # whitened = whiten(pictureArr)
+
+    # Kmeans clustering
+    kArr, label = kmeans2(pictureArr, k)
+
+    # Creates a width*height array reprecenting the image which is filled with corresponding centroid value
+    workaround = 0
+    centroidPicArr = np.zeros(shape=(dimensions[0], dimensions[1]))
+    for i in range(dimensions[0]):
+        for j in range(dimensions[1]):
+            centroidPicArr[i][j] = label[workaround]
+            workaround += 1
+
+    return centroidPicArr,kArr
+
+cluster(misc.imread("images/asdfghjk.png"), 3)
